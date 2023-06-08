90914fb4f9d34979352e5396ba255d5132581a09
TorenR <torentremaine@outlook.com>
2023-06-07 23:20:16 -0400

    feat: create funct for dimensions of sketch grid

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 let etchContainer = document.getElementById("etchContainer");
 color = document.getElementById("colorPick");
 
+function gridMaker(columns) {
+	let etchSketch = document.getElementById("etchSketch");
+	let templateColumns = "1fr";
+
+	for (i = 0; i < columns - 1; i++) {
+		templateColumns += " 1fr";
+	}
+
+	etchSketch.style.gridTemplateColumns = templateColumns;
+}
+
 function makeEtchGrid(rows, columns) {
 	let nNodes = +(rows * columns);
 	for (i = 0; i < nNodes; ++i) {
 		let etchNode = document.createElement("div");
-		etchContainer.append(etchNode);
+		etchSketch.append(etchNode);
 		etchNode.className = "etchNode";
 	}
-}
 
-color.addEventListener("click");
+	gridMaker(columns);
+}
