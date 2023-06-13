4dc87d4daa55726014d7bf8978fe014afc6a6156
TorenR <torentremaine@outlook.com>
2023-06-13 00:29:58 -0400

    Setting up repo

--- script.js
+++ script.js
@@ -7,50 +7,47 @@ colorRadio = document.getElementsByName("color");
 shakeBtn = document.getElementById("shakeBtn");
 colorDisplay = document.getElementById("penColor");
 dimensionDisplay = document.getElementById("boardDims");
-etchBoard = document.getElementById('etchBoard');
+etchBoard = document.getElementById("etchBoard");
 boardNodes = [];
 
-let penColor = '#faf7f8';
+let penColor = "#faf7f8";
 let currentDimension;
 let sketchOn = "Off";
 
-window.addEventListener("load", makeSketchNodes(16))
+window.addEventListener("load", makeSketchNodes(16));
 
 dimensionSlider.addEventListener("change", (e) => {
 	currentDimension = e.target.value;
-	makeSketchNodes(currentDimension)
-	
-	boardNodes = Array.from(document.getElementsByClassName("etchNode"));
-
+	makeSketchNodes(currentDimension);
 	clearBoard();
 });
 
 sketchBoard.addEventListener("mouseover", (e) => {
-	if (sketchOn == "On"){
+	if (sketchOn == "On") {
 		e.target.style.backgroundColor = `${penColor}`;
 	} else {
 		return;
 	}
-})
+});
 
 sketchBoard.addEventListener("click", turnSketchOn);
 
 colorWheel.addEventListener("change", (e) => {
-	penColor = e.target.value
+	penColor = e.target.value;
 });
 
 shakeBtn.addEventListener("click", (e) => {
-	etchBoard.style.animationName = "shakeBoard"
+	etchBoard.style.animationName = "shakeBoard";
 	etchBoard.addEventListener("animationend", (e) => {
 		etchBoard.style.animationName = null;
 		clearBoard();
-	})
-})
+	});
+});
 
 function makeSketchNodes(dimensions) {
 	sketchBoard.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`;
 	sketchBoard.style.gridTemplateRows = `repeat(${dimensions}, 1fr)`;
-	dimensionDisplay.textContent = `${dimensions} x ${dimensions}`
+	dimensionDisplay.textContent = `${dimensions} x ${dimensions}`;
 
 	let nNodes = Math.pow(dimensions, 2);
 	for (i = 0; i < nNodes; i++) {
@@ -58,16 +55,17 @@ function makeSketchNodes(dimensions) {
 		etchNode.classList.add("etchNode");
 		sketchBoard.appendChild(etchNode);
 	}
+	boardNodes = Array.from(document.getElementsByClassName("etchNode"));
 }
 
-function turnSketchOn(){
-	if (sketchOn == "Off"){ 
+function turnSketchOn() {
+	if (sketchOn == "Off") {
 		sketchOn = "On";
 	} else {
 		sketchOn = "Off";
 	}
 }
 
-function clearBoard(){
-	boardNodes.forEach(etch => etch.style.backgroundColor = "rgb(0, 0, 0)");
+function clearBoard() {
+	boardNodes.forEach((etch) => (etch.style.backgroundColor = "rgb(0, 0, 0)"));
 }
